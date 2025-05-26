6805a08e5cd8f2a895dd29553cdfd03ffabec166
wilsnd <wilsondju@gmail.com>
2025-05-26 18:33:55 +1000

    Test cases

--- tests/unit/test_training.py
+++ tests/unit/test_training.py
@@ -2,21 +2,10 @@ import sys
 import os
 import json
 
-# Rood project so can import
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
 
-from model import create_model
 from training import train_model
-
-
-def test_model_creation():
-    """Test model creation works"""
-    print("Testing model creation")
-    model = create_model(vocab_size=1000)
-    assert model is not None
-    assert model.count_parameters() > 0
-    print(f"Model created with {model.count_parameters():,} parameters")
-
+from model import create_model
 
 def test_training_pipeline():
     """Test the training pipeline"""
@@ -36,14 +25,13 @@ def test_training_pipeline():
     # Train the model
     train_model(
         model,
-        "../../processed_data/train_ids.json",
-        "../../processed_data/val_ids.json",
-        epochs=5
+        "mock_data/test_train.json",
+        "mock_data/test_val.json",
+        epochs=1
     )
 
     print("Training test complete!")
 
 
 if __name__ == "__main__":
-    test_model_creation()
     test_training_pipeline()
